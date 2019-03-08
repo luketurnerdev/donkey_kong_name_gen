e52597f230036a1631e5cfdb0dc29c01f3563bc7
Luke Turner <luke@lukedturner.com>
2019-03-08 20:08:09 +1100

    Fixed consonant issue

--- donkey_kong.rb
+++ donkey_kong.rb
@@ -1,15 +1,45 @@
-def dokney()
-    puts "Enter your first name!"
-    first = gets.chomp
 
+VOWEL = ["a","e","i","o","u"]
+
+def donkey()
+    puts "Enter your first name!"
+    first = gets.chomp.downcase
+
+    #make array
+    #go through it and check if vowel
+    #if so, skip
+    #if not, make first = arr[i]
+
+    arr = first.split("")
+    i = 0 
+    while i < arr.length
+        if VOWEL.include?(arr[i]) == false
+            first = arr[i]  
+        end
+        i+=1
+    end
+
+    
     puts "Enter your last name!"
-    last = gets.chomp
+    last = gets.chomp.downcase
+
+    arr = last.split("")
+
+    while i < arr.length
+        if VOWEL.include?(arr[i]) == false
+            last = arr[i]  
+        end
+        i+=1
+    end
 
     string = "Donkey Kong"
+
+
+
     string[0] = first[0]
     string["K"] = last[0]
 
     puts "Your name is #{string}."
 
 end
-dokney
+donkey
